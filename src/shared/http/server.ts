39c2ffdf9853276ef9796e7f66b26207550b90e4
Hariel Santana <hariol.santana138@gmail.com>
2023-01-02 13:02:47 -0300

    feat: validation middleware

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import routes from './routes';
+import AppError from '@shared/erros/AppError';
 
 const app = express();
 
@@ -8,6 +9,17 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(
+  (error: Error, request: express.Request, response: express.Response, next: express.NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+  }
+);
 
 app.listen(3333, () => {
   console.log('Server started on port 3333!');
