ceac342d90edf863701a4b0848df45e93f65924b
Hariel Santana <hariol.santana138@gmail.com>
2023-01-03 18:35:06 -0300

    chore: db connection

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,11 @@
-import express from 'express';
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
+import '@shared/typeorm';
+
 import cors from 'cors';
+
 import routes from './routes';
-import AppError from '@shared/erros/AppError';
+import AppError from '@shared/errors/AppError';
 
 const app = express();
 
@@ -11,14 +15,19 @@ app.use(express.json());
 app.use(routes);
 
 app.use(
-  (error: Error, request: express.Request, response: express.Response, next: express.NextFunction) => {
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
     if (error instanceof AppError) {
       return response.status(error.statusCode).json({
         status: 'error',
         message: error.message,
       });
     }
-  }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
 );
 
 app.listen(3333, () => {
