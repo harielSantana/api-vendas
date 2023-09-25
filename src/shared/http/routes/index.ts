e91fc264b53c4e46fec9121a34367c51ff054b17
Hariel Santana <hariel.oliveira@agenciaalldigital.com>
2023-09-25 18:05:54 -0300

    feat: product routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,8 @@
+import productsRouter from '@modules/products/routes/ProductsRoutes';
 import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
-});
+routes.use('/products', productsRouter);
 
 export default routes;
