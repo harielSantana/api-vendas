e671e195c8b4f5428016bef44e47fe1817a6209a
Hariel Santana <hariel.oliveira@agenciaalldigital.com>
2023-09-27 16:58:19 -0300

    feat: update avatar image

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,11 +1,19 @@
 import { Router } from 'express';
-import UsersController from '../controllers/UserController';
 import { Segments, celebrate } from 'celebrate';
+import multer from 'multer';
 import Joi from 'joi';
+
+import uploadConfig from '@config/upload';
 import isAuthenticated from '../middlewares/isAuthenticated';
+import UsersController from '../controllers/UserController';
+import UserAvatarController from '../controllers/UserAvatarController';
 
 const userRouter = Router();
+
 const userController = new UsersController();
+const usersAvatarController = new UserAvatarController();
+
+const upload = multer(uploadConfig);
 
 userRouter.get('/', isAuthenticated, userController.index);
 
@@ -54,6 +62,13 @@ userRouter.delete(
     },
   }),
   userController.delete,
+);
+
+userRouter.patch(
+  '/avatar',
+  isAuthenticated,
+  upload.single('avatar'),
+  usersAvatarController.update,
 );
 
 export default userRouter;
